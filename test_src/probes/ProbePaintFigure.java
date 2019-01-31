39603a2c7d6bade99e64652ca3d6dcd4a02d701d
MaximProggrammer <maxrocers@gmail.com>
2019-01-31 20:32:20 +0600

    Научились рисовать все фигуры в одном положении

---
+++ test_src/probes/ProbePaintFigure.java
@@ -0,0 +1,77 @@
+package probes;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+public class ProbePaintFigure {
+    public static void main(String[] args) throws Exception {
+        final int SIZE = 330;
+        final int DOT_SIZE = 30;
+        BufferedImage holst = new BufferedImage(SIZE, 600, BufferedImage.TYPE_INT_ARGB);
+
+        {
+            Graphics2D g = (Graphics2D) holst.getGraphics();
+
+            g.setColor(Color.BLACK);
+            g.fillRect(0, 0, 330, 600);
+
+            //draw figure I
+            g.setColor(Color.cyan);
+            g.fillRect(90, DOT_SIZE, DOT_SIZE, DOT_SIZE);
+            g.fillRect(120, DOT_SIZE, DOT_SIZE, DOT_SIZE);
+            g.fillRect(150, DOT_SIZE, DOT_SIZE, DOT_SIZE);
+            g.fillRect(180, DOT_SIZE, DOT_SIZE, DOT_SIZE);
+            //draw figure J
+            g.setColor(Color.blue);
+            g.fillRect(120, DOT_SIZE * 3, DOT_SIZE, DOT_SIZE);
+            g.fillRect(150, DOT_SIZE * 3, DOT_SIZE, DOT_SIZE);
+            g.fillRect(180, DOT_SIZE * 3, DOT_SIZE, DOT_SIZE);
+            g.fillRect(180, DOT_SIZE * 4, DOT_SIZE, DOT_SIZE);
+            //draw figure L
+            g.setColor(Color.orange);
+            g.fillRect(120, DOT_SIZE * 6, DOT_SIZE, DOT_SIZE);
+            g.fillRect(150, DOT_SIZE * 6, DOT_SIZE, DOT_SIZE);
+            g.fillRect(180, DOT_SIZE * 6, DOT_SIZE, DOT_SIZE);
+            g.fillRect(120, DOT_SIZE * 7, DOT_SIZE, DOT_SIZE);
+            //draw figure O
+            g.setColor(Color.yellow);
+            g.fillRect(120, DOT_SIZE * 9, DOT_SIZE, DOT_SIZE);
+            g.fillRect(150, DOT_SIZE * 9, DOT_SIZE, DOT_SIZE);
+            g.fillRect(120, DOT_SIZE * 10, DOT_SIZE, DOT_SIZE);
+            g.fillRect(150, DOT_SIZE * 10, DOT_SIZE, DOT_SIZE);
+            //draw figure S
+            g.setColor(Color.green);
+            g.fillRect(150, DOT_SIZE * 12, DOT_SIZE, DOT_SIZE);
+            g.fillRect(180, DOT_SIZE * 12, DOT_SIZE, DOT_SIZE);
+            g.fillRect(120, DOT_SIZE * 13, DOT_SIZE, DOT_SIZE);
+            g.fillRect(150, DOT_SIZE * 13, DOT_SIZE, DOT_SIZE);
+            //draw figure T
+            g.setColor(Color.magenta);
+            g.fillRect(150, DOT_SIZE * 15, DOT_SIZE, DOT_SIZE);
+            g.fillRect(180, DOT_SIZE * 15, DOT_SIZE, DOT_SIZE);
+            g.fillRect(120, DOT_SIZE * 15, DOT_SIZE, DOT_SIZE);
+            g.fillRect(150, DOT_SIZE * 16, DOT_SIZE, DOT_SIZE);
+            //draw figure Z
+            g.setColor(Color.red);
+            g.fillRect(150, DOT_SIZE * 18, DOT_SIZE, DOT_SIZE);
+            g.fillRect(180, DOT_SIZE * 19, DOT_SIZE, DOT_SIZE);
+            g.fillRect(120, DOT_SIZE * 18, DOT_SIZE, DOT_SIZE);
+            g.fillRect(150, DOT_SIZE * 19, DOT_SIZE, DOT_SIZE);
+
+
+            //draw field
+            for (int i = 0; i < SIZE; i += DOT_SIZE) {
+                g.setColor(Color.GRAY);
+                for (int j = 0; j < 600; j += DOT_SIZE) {
+                    g.drawLine(i, 0, i, 600);
+                    g.drawLine(0, j, SIZE, j);
+                }
+            }
+        }
+
+
+        ImageIO.write(holst, "png", new File("build/holst.png"));
+    }
+}
