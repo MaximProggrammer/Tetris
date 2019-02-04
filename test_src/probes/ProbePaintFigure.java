a2350d3acb7de1cbacc9751d9f65f35cdbd9c458
MaximProggrammer <maxrocers@gmail.com>
2019-02-05 01:09:39 +0600

    Научились рисовать на холсте все возможные поворы фигур

--- test_src/probes/ProbePaintFigure.java
+++ test_src/probes/ProbePaintFigure.java
@@ -17,6 +17,8 @@ public class ProbePaintFigure {
             g.setColor(Color.BLACK);
             g.fillRect(0, 0, 330, 600);
 
+            /*
+
             //draw figure I
             g.setColor(Color.cyan);
             g.fillRect(90, DOT_SIZE, DOT_SIZE, DOT_SIZE);
@@ -58,6 +60,90 @@ public class ProbePaintFigure {
             g.fillRect(150, DOT_SIZE * 18, DOT_SIZE, DOT_SIZE);
             g.fillRect(180, DOT_SIZE * 19, DOT_SIZE, DOT_SIZE);
             g.fillRect(120, DOT_SIZE * 18, DOT_SIZE, DOT_SIZE);
+            g.fillRect(150, DOT_SIZE * 19, DOT_SIZE, DOT_SIZE);
+
+            */
+
+            //move figure to rotate
+
+            // figure I2
+            g.setColor(Color.red);
+            g.fillRect(120, DOT_SIZE, DOT_SIZE, DOT_SIZE);
+            g.fillRect(120, DOT_SIZE*2, DOT_SIZE, DOT_SIZE);
+            g.fillRect(120, DOT_SIZE*3, DOT_SIZE, DOT_SIZE);
+            g.fillRect(120, DOT_SIZE*4, DOT_SIZE, DOT_SIZE);
+            // figure I3
+            g.setColor(Color.red);
+            g.fillRect(120, DOT_SIZE, DOT_SIZE, DOT_SIZE);
+            g.fillRect(90, DOT_SIZE, DOT_SIZE, DOT_SIZE);
+            g.fillRect(60, DOT_SIZE, DOT_SIZE, DOT_SIZE);
+            g.fillRect(30, DOT_SIZE, DOT_SIZE, DOT_SIZE);
+
+            // figure J2
+            g.setColor(Color.blue);
+            g.fillRect(180, DOT_SIZE * 2, DOT_SIZE, DOT_SIZE);
+            g.fillRect(180, DOT_SIZE * 3, DOT_SIZE, DOT_SIZE);
+            g.fillRect(180, DOT_SIZE * 4, DOT_SIZE, DOT_SIZE);
+            g.fillRect(150, DOT_SIZE * 4, DOT_SIZE, DOT_SIZE);
+            // figure J3
+            g.setColor(Color.blue);
+            g.fillRect(120, DOT_SIZE * 4, DOT_SIZE, DOT_SIZE);
+            g.fillRect(150, DOT_SIZE * 4, DOT_SIZE, DOT_SIZE);
+            g.fillRect(120, DOT_SIZE * 3, DOT_SIZE, DOT_SIZE);
+            g.fillRect(180, DOT_SIZE * 4, DOT_SIZE, DOT_SIZE);
+            // figure J4
+            g.setColor(Color.blue);
+            g.fillRect(180, DOT_SIZE * 2, DOT_SIZE, DOT_SIZE);
+            g.fillRect(180, DOT_SIZE * 3, DOT_SIZE, DOT_SIZE);
+            g.fillRect(180, DOT_SIZE * 4, DOT_SIZE, DOT_SIZE);
+            g.fillRect(210, DOT_SIZE * 2, DOT_SIZE, DOT_SIZE);
+            // figure L2
+            g.setColor(Color.orange);
+            g.fillRect(180, DOT_SIZE * 2, DOT_SIZE, DOT_SIZE);
+            g.fillRect(180, DOT_SIZE * 3, DOT_SIZE, DOT_SIZE);
+            g.fillRect(180, DOT_SIZE * 4, DOT_SIZE, DOT_SIZE);
+            g.fillRect(150, DOT_SIZE * 2, DOT_SIZE, DOT_SIZE);
+            // figure L3
+            g.setColor(Color.orange);
+            g.fillRect(120, DOT_SIZE * 4, DOT_SIZE, DOT_SIZE);
+            g.fillRect(150, DOT_SIZE * 4, DOT_SIZE, DOT_SIZE);
+            g.fillRect(180, DOT_SIZE * 3, DOT_SIZE, DOT_SIZE);
+            g.fillRect(180, DOT_SIZE * 4, DOT_SIZE, DOT_SIZE);
+            // figure L4
+            g.setColor(Color.orange);
+            g.fillRect(180, DOT_SIZE * 2, DOT_SIZE, DOT_SIZE);
+            g.fillRect(180, DOT_SIZE * 3, DOT_SIZE, DOT_SIZE);
+            g.fillRect(180, DOT_SIZE * 4, DOT_SIZE, DOT_SIZE);
+            g.fillRect(210, DOT_SIZE * 4, DOT_SIZE, DOT_SIZE);
+            // figure S2
+            g.setColor(Color.green);
+            g.fillRect(150, DOT_SIZE * 11, DOT_SIZE, DOT_SIZE);
+            g.fillRect(180, DOT_SIZE * 12, DOT_SIZE, DOT_SIZE);
+            g.fillRect(180, DOT_SIZE * 13, DOT_SIZE, DOT_SIZE);
+            g.fillRect(150, DOT_SIZE * 12, DOT_SIZE, DOT_SIZE);
+            // figure T2
+            g.setColor(Color.magenta);
+            g.fillRect(150, DOT_SIZE * 15, DOT_SIZE, DOT_SIZE);
+            g.fillRect(120, DOT_SIZE * 15, DOT_SIZE, DOT_SIZE);
+            g.fillRect(150, DOT_SIZE * 14, DOT_SIZE, DOT_SIZE);
+            g.fillRect(150, DOT_SIZE * 16, DOT_SIZE, DOT_SIZE);
+            // figure T3
+            g.setColor(Color.magenta);
+            g.fillRect(150, DOT_SIZE * 15, DOT_SIZE, DOT_SIZE);
+            g.fillRect(180, DOT_SIZE * 15, DOT_SIZE, DOT_SIZE);
+            g.fillRect(120, DOT_SIZE * 15, DOT_SIZE, DOT_SIZE);
+            g.fillRect(150, DOT_SIZE * 14, DOT_SIZE, DOT_SIZE);
+            // fugure T4
+            g.setColor(Color.magenta);
+            g.fillRect(150, DOT_SIZE * 15, DOT_SIZE, DOT_SIZE);
+            g.fillRect(180, DOT_SIZE * 15, DOT_SIZE, DOT_SIZE);
+            g.fillRect(150, DOT_SIZE * 14, DOT_SIZE, DOT_SIZE);
+            g.fillRect(150, DOT_SIZE * 16, DOT_SIZE, DOT_SIZE);
+            // figure Z2
+            g.setColor(Color.red);
+            g.fillRect(180, DOT_SIZE * 17, DOT_SIZE, DOT_SIZE);
+            g.fillRect(150, DOT_SIZE * 18, DOT_SIZE, DOT_SIZE);
+            g.fillRect(180, DOT_SIZE * 18, DOT_SIZE, DOT_SIZE);
             g.fillRect(150, DOT_SIZE * 19, DOT_SIZE, DOT_SIZE);
 
 
