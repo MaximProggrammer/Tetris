5298f346c088e4f920c9796cff1a66094e66acae
MaximProggrammer <maxrocers@gmail.com>
2019-02-07 02:09:51 +0600

    Обновили класс менеджера фигур, добавили все возможные повороты фигур

--- src/figure_manager/FigureManager.java
+++ src/figure_manager/FigureManager.java
@@ -55,6 +55,153 @@ public class FigureManager {
                 figure[3][0] = 150; figure[3][1] = 0; figure[3][2] = 30; figure[3][3] = 30;
                 figure[4][0] = 255; figure[4][1] = 0; figure[4][2] = 0;
                 break;
+            case I2:
+                figure[0][0] = 120; figure[0][1] = 30; figure[0][2] = 30; figure[0][3] = 30;
+                figure[1][0] = 120; figure[1][1] = 60; figure[1][2] = 30; figure[1][3] = 30;
+                figure[2][0] = 120; figure[2][1] = 90; figure[2][2] = 30; figure[2][3] = 30;
+                figure[3][0] = 120; figure[3][1] = 120; figure[3][2] = 30; figure[3][3] = 30;
+                figure[4][0] = 0; figure[4][1] = 255; figure[4][2] = 255;
+                break;
+            case J2:
+                figure[0][0] = 180; figure[0][1] = 30; figure[0][2] = 30; figure[0][3] = 30;
+                figure[1][0] = 180; figure[1][1] = 60; figure[1][2] = 30; figure[1][3] = 30;
+                figure[2][0] = 180; figure[2][1] = 90; figure[2][2] = 30; figure[2][3] = 30;
+                figure[3][0] = 150; figure[3][1] = 90; figure[3][2] = 30; figure[3][3] = 30;
+                figure[4][0] = 0; figure[4][1] = 0; figure[4][2] = 255;
+                break;
+            case L2:
+                figure[0][0] = 180; figure[0][1] = 30; figure[0][2] = 30; figure[0][3] = 30;
+                figure[1][0] = 180; figure[1][1] = 60; figure[1][2] = 30; figure[1][3] = 30;
+                figure[2][0] = 180; figure[2][1] = 90; figure[2][2] = 30; figure[2][3] = 30;
+                figure[3][0] = 150; figure[3][1] = 30; figure[3][2] = 30; figure[3][3] = 30;
+                figure[4][0] = 255; figure[4][1] = 165; figure[4][2] = 0;
+                break;
+            case O2:
+                figure[0][0] = 120; figure[0][1] = 30; figure[0][2] = 30; figure[0][3] = 30;
+                figure[1][0] = 150; figure[1][1] = 30; figure[1][2] = 30; figure[1][3] = 30;
+                figure[2][0] = 120; figure[2][1] = 0; figure[2][2] = 30; figure[2][3] = 30;
+                figure[3][0] = 150; figure[3][1] = 0; figure[3][2] = 30; figure[3][3] = 30;
+                figure[4][0] = 255; figure[4][1] = 255; figure[4][2] = 0;
+                break;
+            case S2:
+                figure[0][0] = 150; figure[0][1] = 30; figure[0][2] = 30; figure[0][3] = 30;
+                figure[1][0] = 180; figure[1][1] = 60; figure[1][2] = 30; figure[1][3] = 30;
+                figure[2][0] = 180; figure[2][1] = 90; figure[2][2] = 30; figure[2][3] = 30;
+                figure[3][0] = 150; figure[3][1] = 60; figure[3][2] = 30; figure[3][3] = 30;
+                figure[4][0] = 0; figure[4][1] = 128; figure[4][2] = 0;
+                break;
+            case T2:
+                figure[0][0] = 150; figure[0][1] = 60; figure[0][2] = 30; figure[0][3] = 30;
+                figure[1][0] = 120; figure[1][1] = 60; figure[1][2] = 30; figure[1][3] = 30;
+                figure[2][0] = 150; figure[2][1] = 30; figure[2][2] = 30; figure[2][3] = 30;
+                figure[3][0] = 150; figure[3][1] = 90; figure[3][2] = 30; figure[3][3] = 30;
+                figure[4][0] = 128; figure[4][1] = 0; figure[4][2] = 128;
+                break;
+            case Z2:
+                figure[0][0] = 180; figure[0][1] = 30; figure[0][2] = 30; figure[0][3] = 30;
+                figure[1][0] = 150; figure[1][1] = 60; figure[1][2] = 30; figure[1][3] = 30;
+                figure[2][0] = 180; figure[2][1] = 60; figure[2][2] = 30; figure[2][3] = 30;
+                figure[3][0] = 150; figure[3][1] = 90; figure[3][2] = 30; figure[3][3] = 30;
+                figure[4][0] = 255; figure[4][1] = 0; figure[4][2] = 0;
+                break;
+            case I3:
+                figure[0][0] = 120; figure[0][1] = 30; figure[0][2] = 30; figure[0][3] = 30;
+                figure[1][0] = 90; figure[1][1] = 30; figure[1][2] = 30; figure[1][3] = 30;
+                figure[2][0] = 60; figure[2][1] = 30; figure[2][2] = 30; figure[2][3] = 30;
+                figure[3][0] = 30; figure[3][1] = 30; figure[3][2] = 30; figure[3][3] = 30;
+                figure[4][0] = 0; figure[4][1] = 255; figure[4][2] = 255;
+                break;
+            case J3:
+                figure[0][0] = 120; figure[0][1] = 60; figure[0][2] = 30; figure[0][3] = 30;
+                figure[1][0] = 150; figure[1][1] = 60; figure[1][2] = 30; figure[1][3] = 30;
+                figure[2][0] = 120; figure[2][1] = 30; figure[2][2] = 30; figure[2][3] = 30;
+                figure[3][0] = 180; figure[3][1] = 60; figure[3][2] = 30; figure[3][3] = 30;
+                figure[4][0] = 0; figure[4][1] = 0; figure[4][2] = 255;
+                break;
+            case L3:
+                figure[0][0] = 120; figure[0][1] = 60; figure[0][2] = 30; figure[0][3] = 30;
+                figure[1][0] = 150; figure[1][1] = 60; figure[1][2] = 30; figure[1][3] = 30;
+                figure[2][0] = 180; figure[2][1] = 30; figure[2][2] = 30; figure[2][3] = 30;
+                figure[3][0] = 180; figure[3][1] = 60; figure[3][2] = 30; figure[3][3] = 30;
+                figure[4][0] = 255; figure[4][1] = 165; figure[4][2] = 0;
+                break;
+            case O3:
+                figure[0][0] = 120; figure[0][1] = 30; figure[0][2] = 30; figure[0][3] = 30;
+                figure[1][0] = 150; figure[1][1] = 30; figure[1][2] = 30; figure[1][3] = 30;
+                figure[2][0] = 120; figure[2][1] = 0; figure[2][2] = 30; figure[2][3] = 30;
+                figure[3][0] = 150; figure[3][1] = 0; figure[3][2] = 30; figure[3][3] = 30;
+                figure[4][0] = 255; figure[4][1] = 255; figure[4][2] = 0;
+                break;
+            case S3:
+                figure[0][0] = 150; figure[0][1] = 30; figure[0][2] = 30; figure[0][3] = 30;
+                figure[1][0] = 180; figure[1][1] = 30; figure[1][2] = 30; figure[1][3] = 30;
+                figure[2][0] = 120; figure[2][1] = 0; figure[2][2] = 30; figure[2][3] = 30;
+                figure[3][0] = 150; figure[3][1] = 0; figure[3][2] = 30; figure[3][3] = 30;
+                figure[4][0] = 0; figure[4][1] = 128; figure[4][2] = 0;
+                break;
+            case T3:
+                figure[0][0] = 150; figure[0][1] = 60; figure[0][2] = 30; figure[0][3] = 30;
+                figure[1][0] = 180; figure[1][1] = 60; figure[1][2] = 30; figure[1][3] = 30;
+                figure[2][0] = 120; figure[2][1] = 60; figure[2][2] = 30; figure[2][3] = 30;
+                figure[3][0] = 150; figure[3][1] = 30; figure[3][2] = 30; figure[3][3] = 30;
+                figure[4][0] = 128; figure[4][1] = 0; figure[4][2] = 128;
+                break;
+            case Z3:
+                figure[0][0] = 150; figure[0][1] = 30; figure[0][2] = 30; figure[0][3] = 30;
+                figure[1][0] = 180; figure[1][1] = 0; figure[1][2] = 30; figure[1][3] = 30;
+                figure[2][0] = 120; figure[2][1] = 30; figure[2][2] = 30; figure[2][3] = 30;
+                figure[3][0] = 150; figure[3][1] = 0; figure[3][2] = 30; figure[3][3] = 30;
+                figure[4][0] = 255; figure[4][1] = 0; figure[4][2] = 0;
+                break;
+            case I4:
+                figure[0][0] = 120; figure[0][1] = 30; figure[0][2] = 30; figure[0][3] = 30;
+                figure[1][0] = 120; figure[1][1] = 60; figure[1][2] = 30; figure[1][3] = 30;
+                figure[2][0] = 120; figure[2][1] = 90; figure[2][2] = 30; figure[2][3] = 30;
+                figure[3][0] = 120; figure[3][1] = 120; figure[3][2] = 30; figure[3][3] = 30;
+                figure[4][0] = 0; figure[4][1] = 255; figure[4][2] = 255;
+                break;
+            case J4:
+                figure[0][0] = 180; figure[0][1] = 30; figure[0][2] = 30; figure[0][3] = 30;
+                figure[1][0] = 180; figure[1][1] = 60; figure[1][2] = 30; figure[1][3] = 30;
+                figure[2][0] = 180; figure[2][1] = 90; figure[2][2] = 30; figure[2][3] = 30;
+                figure[3][0] = 210; figure[3][1] = 30; figure[3][2] = 30; figure[3][3] = 30;
+                figure[4][0] = 0; figure[4][1] = 0; figure[4][2] = 255;
+                break;
+            case L4:
+                figure[0][0] = 180; figure[0][1] = 30; figure[0][2] = 30; figure[0][3] = 30;
+                figure[1][0] = 180; figure[1][1] = 60; figure[1][2] = 30; figure[1][3] = 30;
+                figure[2][0] = 180; figure[2][1] = 90; figure[2][2] = 30; figure[2][3] = 30;
+                figure[3][0] = 210; figure[3][1] = 90; figure[3][2] = 30; figure[3][3] = 30;
+                figure[4][0] = 255; figure[4][1] = 165; figure[4][2] = 0;
+                break;
+            case O4:
+                figure[0][0] = 120; figure[0][1] = 30; figure[0][2] = 30; figure[0][3] = 30;
+                figure[1][0] = 150; figure[1][1] = 30; figure[1][2] = 30; figure[1][3] = 30;
+                figure[2][0] = 120; figure[2][1] = 0; figure[2][2] = 30; figure[2][3] = 30;
+                figure[3][0] = 150; figure[3][1] = 0; figure[3][2] = 30; figure[3][3] = 30;
+                figure[4][0] = 255; figure[4][1] = 255; figure[4][2] = 0;
+                break;
+            case S4:
+                figure[0][0] = 150; figure[0][1] = 30; figure[0][2] = 30; figure[0][3] = 30;
+                figure[1][0] = 180; figure[1][1] = 60; figure[1][2] = 30; figure[1][3] = 30;
+                figure[2][0] = 180; figure[2][1] = 90; figure[2][2] = 30; figure[2][3] = 30;
+                figure[3][0] = 150; figure[3][1] = 60; figure[3][2] = 30; figure[3][3] = 30;
+                figure[4][0] = 0; figure[4][1] = 128; figure[4][2] = 0;
+                break;
+            case T4:
+                figure[0][0] = 150; figure[0][1] = 60; figure[0][2] = 30; figure[0][3] = 30;
+                figure[1][0] = 180; figure[1][1] = 60; figure[1][2] = 30; figure[1][3] = 30;
+                figure[2][0] = 150; figure[2][1] = 30; figure[2][2] = 30; figure[2][3] = 30;
+                figure[3][0] = 150; figure[3][1] = 90; figure[3][2] = 30; figure[3][3] = 30;
+                figure[4][0] = 128; figure[4][1] = 0; figure[4][2] = 128;
+                break;
+            case Z4:
+                figure[0][0] = 180; figure[0][1] = 30; figure[0][2] = 30; figure[0][3] = 30;
+                figure[1][0] = 150; figure[1][1] = 60; figure[1][2] = 30; figure[1][3] = 30;
+                figure[2][0] = 180; figure[2][1] = 60; figure[2][2] = 30; figure[2][3] = 30;
+                figure[3][0] = 150; figure[3][1] = 90; figure[3][2] = 30; figure[3][3] = 30;
+                figure[4][0] = 255; figure[4][1] = 0; figure[4][2] = 0;
+                break;
         }
 
         return figure;
--- src/figure_manager/Figures.java
+++ src/figure_manager/Figures.java
@@ -1,5 +1,11 @@
 package figure_manager;
 
 public enum Figures {
-    I,J,L,O,S,T,Z;
+    I,I2,I3,I4,
+    J,J2,J3,J4,
+    L,L2,L3,L4,
+    O,O2,O3,O4,
+    S,S2,S3,S4,
+    T,T2,T3,T4,
+    Z,Z2,Z3,Z4;
 }
